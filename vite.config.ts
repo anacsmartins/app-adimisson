612cefde447ef678871d60bfe379f04011931907
anacarolinamartins <anamartins.sistemas@gmail.com>
2024-09-25 19:46:05 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
-// @charset "UTF-8";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
   server: {
-    port: 3001,
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3001,
+    host: true,
   },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
